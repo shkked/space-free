ee01e04b61761d801cb1f9484105568da296947a
kenny <pletnev735@gmail.com>
2024-11-01 22:41:21 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./style/style.css">
+    <title>Инфраструктура</title>
+</head>
+<body>
+<main>
+    <div class="container">
+        <section class="section">
+            <div class="section__title">
+                <p class="section__under-title">03</p>
+                <h1 class="h1">Инфраструктура</h1>
+            </div>
+            <div class="section__inner">
+                <a href="#" target="_blank" class="section__pool section__grid-item">
+                    <img class="section__img" src="./static/img/pool.jpg" alt="pool-img">
+                    <div class="section__label">
+                        <img class="section__label-icon" src="./static/icons/pool.svg" alt="pool-icon">
+                        <p class="section__label-text">Бассейн</p>
+                    </div>
+                </a>
+                <a href="#" target="_blank" class="section__table section__grid-item">
+                    <img class="section__img" src="./static/img/table.jpg" alt="table-img">
+                    <div class="section__label">
+                        <img class="section__label-icon" src="./static/icons/table.svg" alt="table-icon">
+                        <p class="section__label-text">Шведский стол</p>
+                    </div>
+                </a>
+                <a href="#" target="_blank" class="section__fitness section__grid-item ">
+                    <img class="section__img" src="./static/img/fitness.jpg" alt="fitness-img">
+                    <div class="section__label">
+                        <img class="section__label-icon" src="./static/icons/fitness.svg" alt="fitness-icon">
+                        <p class="section__label-text">Фитнес</p>
+                    </div>
+                </a>
+                <div class="section__other section__grid-item">
+                    <div class="section__other-item section__grid-item visible-mobile">
+                        <a href="#" target="_blank" class="section__other-item section__grid-item">
+                            <div class="section__label visible-mobile">
+                                <img class="section__label-icon" src="./static/icons/parking.svg" alt="parking-icon">
+                            </div>
+                            <p class="section__label-text">Паркинг</p>
+                        </a>
+                    </div>
+                    <div class="section__other-item section__grid-item visible-mobile">
+                        <a href="#" target="_blank" class="section__other-item section__grid-item">
+                            <div class="section__label visible-mobile">
+                                <img class="section__label-icon" src="./static/icons/shops.svg" alt="shops-icon">
+                            </div>
+                            <p class="section__label-text">Магазины</p>
+                        </a>
+                    </div>
+                    <div class="section__other-item section__grid-item visible-mobile">
+                        <a href="#" target="_blank" class="section__other-item section__grid-item">
+                            <div class="section__label visible-mobile">
+                                <img class="section__label-icon" src="./static/icons/conf.svg" alt="conf-icon">
+                            </div>
+                            <p class="section__label-text">Конференц-залы</p>
+                        </a>
+                    </div>
+                    <a href="#" target="_blank" class="section__other-item section__label section__grid-item hidden-mobile">
+                        <img class="section__label-icon" src="./static/icons/parking.svg" alt="parking-icon">
+                        <p class="section__label-text">Паркинг</p>
+                    </a>
+                    <a href="#" target="_blank" class="section__other-item section__label section__grid-item hidden-mobile" >
+                        <img class="section__label-icon" src="./static/icons/beautiful.svg" alt="beautiful-icon">
+                        <p class="section__label-text hidden-mobile">Красота и здоровье</p>
+                    </a>
+                    <a href="#" target="_blank" class="section__other-item section__label section__grid-item hidden-mobile">
+                        <img class="section__label-icon" src="./static/icons/shops.svg" alt="shops-icon">
+                        <p class="section__label-text hidden-mobile">Магазины</p>
+                    </a>
+                    <div class="section__grid-container">
+                        <a href="#" target="_blank" class="section__other-item section__label section__grid-item">
+                            <img class="section__label-icon" src="./static/icons/rest.svg" alt="restaurant-icon">
+                            <p class="section__label-text">Кафе и рестораны</p>
+                        </a>
+                        <a href="#" target="_blank" class="section__other-item section__label section__grid-item hidden-mobile">
+                            <img class="section__label-icon" src="./static/icons/conf.svg" alt="conference-icon">
+                            <p class="section__label-text">Конференц-залы</p>
+                        </a>
+                        <a href="#" target="_blank" class="section__other-item section__label section__grid-item visible-mobile">
+                            <img class="section__label-icon" src="./static/icons/beautiful.svg" alt="beautiful-icon">
+                            <p class="section__label-text">Красота и здоровье</p>
+                        </a>
+                    </div>  
+                </div>
+                <div class="section__inner-bottom">
+                    <a href="#" target="_blank" class="section__grid-bottom section__grid-item section__label">
+                        <p class="section__label-text">Мир VR</p>
+                    </a>
+                    <a href="#" target="_blank" class="section__grid-bottom section__grid-item section__label">
+                        <p class="section__label-text">Каток</p>
+                    </a>
+                    <a href="#" target="_blank" class="section__grid-bottom section__grid-item section__label">
+                        <p class="section__label-text">ТРК “Галерея”</p>
+                    </a>
+                    <a href="#" target="_blank" class="section__grid-bottom section__grid-item section__label">
+                        <p class="section__label-text">Парк аттракционов</p>
+                    </a>
+                    <a href="#" target="_blank" class="section__grid-bottom section__grid-item section__label">
+                        <p class="section__label-text">Кинотеатр</p>
+                    </a>
+                </div>
+            </div>
+        </section>
+    </div>
+</main>
+</body>
+</html>
